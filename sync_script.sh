70912ec5965c3538ee957b0e5cc9ad5c5a33d6c2
K R I S H N A <116506067+pure-soul-kk@users.noreply.github.com>
2026-08-13 23:06:25 +0530

    Update sync_script.sh with new repository picks

--- sync_script.sh
+++ sync_script.sh
@@ -10,6 +10,11 @@ set -euo pipefail
 
 REPOPICK="./vendor/custom/build/tools/repopick.py"
 
+if [[ ! -f "${REPOPICK}" ]]; then
+    echo "Error: repopick.py not found at ${REPOPICK}" >&2
+    exit 1
+fi
+
 # ---------------------------------------------------------------------------
 # extra_deps: rm -rf path && git clone --depth=1 url -b branch path
 # ---------------------------------------------------------------------------
@@ -22,6 +27,7 @@ clone_dep() {
 }
 
 clone_dep "https://github.com/PixelOS-17/android_vendor_lineage" "seventeen" "vendor/lineage"
+clone_dep "https://github.com/PixelOS-17/android_system_fs_fs_mgr" "seventeen-2" "system/fs/fs_mgr"
 
 # ---------------------------------------------------------------------------
 # picks: repopick.py -P id   (comment = originating repo, for reference)
@@ -37,16 +43,16 @@ pick 9697 "PixelOS-AOSP/android_external_wpa_supplicant_8"
 pick 9783 "PixelOS-AOSP/android_external_bouncycastle"
 pick 9775 "PixelOS-AOSP/android_art"
 pick 9746 "PixelOS-AOSP/android_system_memory_libmeminfo"
-pick 9282 "PixelOS-AOSP/android_frameworks_av"
+pick 10441 "PixelOS-AOSP/android_frameworks_av"
 pick 9633 "PixelOS-AOSP/android_external_zstd"
-# pick 9752 "PixelOS-AOSP/android_vendor_custom"
+pick 10488 "PixelOS-AOSP/android_vendor_custom"
 pick 9604 "PixelOS-AOSP/android_bionic"
 pick 9578 "PixelOS-AOSP/android_bootable_deprecated-ota"
 pick 9774 "PixelOS-AOSP/android_bootable_recovery"
-pick 9639 "PixelOS-AOSP/android_build"
+pick 10482 "PixelOS-AOSP/android_build"
 pick 9212 "PixelOS-AOSP/android_build_soong"
 pick 9605 "PixelOS-AOSP/android_development"
-pick 9644 "PixelOS-AOSP/android_external_avb"
+pick 9580 "PixelOS-AOSP/android_external_avb"
 pick 9595 "PixelOS-AOSP/android_external_dng_sdk"
 pick 9575 "PixelOS-AOSP/android_external_dtc"
 pick 9597 "PixelOS-AOSP/android_external_e2fsprogs"
@@ -54,7 +60,7 @@ pick 9559 "PixelOS-AOSP/android_external_gptfdisk"
 pick 9606 "PixelOS-AOSP/android_external_libjxl"
 pick 9583 "PixelOS-AOSP/android_external_mksh"
 pick 9599 "PixelOS-AOSP/android_external_tinycompress"
-# pick 9837 "PixelOS-AOSP/android_frameworks_base"
+pick 9837 "PixelOS-AOSP/android_frameworks_base"
 pick 9379 "PixelOS-AOSP/android_frameworks_native"
 pick 9485 "PixelOS-AOSP/android_frameworks_opt_telephony"
 pick 9552 "PixelOS-AOSP/android_hardware_broadcom_libbt"
@@ -76,7 +82,6 @@ pick 9536 "PixelOS-AOSP/android_packages_services_Telephony"
 pick 9609 "PixelOS-AOSP/android_system_bpf"
 pick 9610 "PixelOS-AOSP/android_system_chre"
 pick 9756 "PixelOS-AOSP/android_system_core"
-pick 9456 "PixelOS-AOSP/android_system_fs_fs_mgr"
 pick 9611 "PixelOS-AOSP/android_system_libziparchive"
 pick 9585 "PixelOS-AOSP/android_system_logging"
 pick 9603 "PixelOS-AOSP/android_system_media"
