a45837e6c2ca8b3266ed79bff54f42039d09c3e9
K R I S H N A <krishnakripa34567@gmail.com>
2026-08-14 08:18:44 +0530

    pick -> pull
    Signed-off-by: K R I S H N A <krishnakripa34567@gmail.com>

--- sync_script.sh
+++ sync_script.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 #
-# Re-creates the "extra_deps" repos and applies the "picks" (repopick)
+# Re-creates the "extra_deps" repos and applies the "pulls" (repopick)
 # entries from the manifest.
 #
 # Usage: run from the root of your ROM source tree.
-#   ./setup_deps_and_picks.sh
+#   ./setup_deps_and_pulls.sh
 
 set -euo pipefail
 
@@ -30,68 +30,68 @@ clone_dep "https://github.com/PixelOS-17/android_vendor_lineage" "seventeen" "ve
 clone_dep "https://github.com/PixelOS-17/android_system_fs_fs_mgr" "seventeen-2" "system/fs/fs_mgr"
 
 # ---------------------------------------------------------------------------
-# picks: repopick.py -P id   (comment = originating repo, for reference)
+# pull: repopick.py -p id   (comment = originating repo, for reference)
 # ---------------------------------------------------------------------------
 
-pick() {
+pull() {
     local id="$1" comment="$2"
-    echo "==> [picks] #${id}  (${comment})"
+    echo "==> [pull] #${id}  (${comment})"
     "${REPOPICK}" -p "${id}"
 }
 
-pick 9697 "PixelOS-AOSP/android_external_wpa_supplicant_8"
-pick 9783 "PixelOS-AOSP/android_external_bouncycastle"
-pick 9775 "PixelOS-AOSP/android_art"
-pick 9746 "PixelOS-AOSP/android_system_memory_libmeminfo"
-pick 10441 "PixelOS-AOSP/android_frameworks_av"
-pick 9633 "PixelOS-AOSP/android_external_zstd"
-pick 10488 "PixelOS-AOSP/android_vendor_custom"
-pick 9604 "PixelOS-AOSP/android_bionic"
-pick 9578 "PixelOS-AOSP/android_bootable_deprecated-ota"
-pick 9774 "PixelOS-AOSP/android_bootable_recovery"
-pick 10482 "PixelOS-AOSP/android_build"
-pick 9212 "PixelOS-AOSP/android_build_soong"
-pick 9605 "PixelOS-AOSP/android_development"
-pick 9580 "PixelOS-AOSP/android_external_avb"
-pick 9595 "PixelOS-AOSP/android_external_dng_sdk"
-pick 9575 "PixelOS-AOSP/android_external_dtc"
-pick 9597 "PixelOS-AOSP/android_external_e2fsprogs"
-pick 9559 "PixelOS-AOSP/android_external_gptfdisk"
-pick 9606 "PixelOS-AOSP/android_external_libjxl"
-pick 9583 "PixelOS-AOSP/android_external_mksh"
-pick 9599 "PixelOS-AOSP/android_external_tinycompress"
-pick 9837 "PixelOS-AOSP/android_frameworks_base"
-pick 9379 "PixelOS-AOSP/android_frameworks_native"
-pick 9485 "PixelOS-AOSP/android_frameworks_opt_telephony"
-pick 9552 "PixelOS-AOSP/android_hardware_broadcom_libbt"
-pick 9436 "PixelOS-AOSP/android_hardware_google_pixel"
-pick 9440 "PixelOS-AOSP/android_hardware_google_pixel-sepolicy"
-pick 9748 "PixelOS-AOSP/android_hardware_interfaces"
-pick 9601 "PixelOS-AOSP/android_hardware_libhardware"
-pick 9607 "PixelOS-AOSP/android_hardware_nxp_nfc"
-pick 9589 "PixelOS-AOSP/android_hardware_st_nfc"
-pick 9508 "PixelOS-AOSP/android_kernel_configs"
-pick 9045 "PixelOS-AOSP/android_packages_apps_Settings"
-pick 9530 "PixelOS-AOSP/android_packages_modules_Bluetooth"
-pick 9518 "PixelOS-AOSP/android_packages_modules_Connectivity"
-pick 9543 "PixelOS-AOSP/android_packages_modules_Permission"
-pick 9608 "PixelOS-AOSP/android_packages_modules_Wifi"
-pick 9540 "PixelOS-AOSP/android_packages_modules_adb"
-pick 9592 "PixelOS-AOSP/android_packages_services_Telecomm"
-pick 9536 "PixelOS-AOSP/android_packages_services_Telephony"
-pick 9609 "PixelOS-AOSP/android_system_bpf"
-pick 9610 "PixelOS-AOSP/android_system_chre"
-pick 9756 "PixelOS-AOSP/android_system_core"
-pick 9611 "PixelOS-AOSP/android_system_libziparchive"
-pick 9585 "PixelOS-AOSP/android_system_logging"
-pick 9603 "PixelOS-AOSP/android_system_media"
-pick 9523 "PixelOS-AOSP/android_system_netd"
-pick 9830 "PixelOS-AOSP/android_system_security"
-pick 9397 "PixelOS-AOSP/android_system_sepolicy"
-pick 9497 "PixelOS-AOSP/android_system_update_engine"
-pick 9413 "PixelOS-AOSP/android_system_vold"
+pull 9697 "PixelOS-AOSP/android_external_wpa_supplicant_8"
+pull 9783 "PixelOS-AOSP/android_external_bouncycastle"
+pull 9775 "PixelOS-AOSP/android_art"
+pull 9746 "PixelOS-AOSP/android_system_memory_libmeminfo"
+pull 10441 "PixelOS-AOSP/android_frameworks_av"
+pull 9633 "PixelOS-AOSP/android_external_zstd"
+pull 10488 "PixelOS-AOSP/android_vendor_custom"
+pull 9604 "PixelOS-AOSP/android_bionic"
+pull 9578 "PixelOS-AOSP/android_bootable_deprecated-ota"
+pull 9774 "PixelOS-AOSP/android_bootable_recovery"
+pull 10482 "PixelOS-AOSP/android_build"
+pull 9212 "PixelOS-AOSP/android_build_soong"
+pull 9605 "PixelOS-AOSP/android_development"
+pull 9580 "PixelOS-AOSP/android_external_avb"
+pull 9595 "PixelOS-AOSP/android_external_dng_sdk"
+pull 9575 "PixelOS-AOSP/android_external_dtc"
+pull 9597 "PixelOS-AOSP/android_external_e2fsprogs"
+pull 9559 "PixelOS-AOSP/android_external_gptfdisk"
+pull 9606 "PixelOS-AOSP/android_external_libjxl"
+pull 9583 "PixelOS-AOSP/android_external_mksh"
+pull 9599 "PixelOS-AOSP/android_external_tinycompress"
+pull 9837 "PixelOS-AOSP/android_frameworks_base"
+pull 9379 "PixelOS-AOSP/android_frameworks_native"
+pull 9485 "PixelOS-AOSP/android_frameworks_opt_telephony"
+pull 9552 "PixelOS-AOSP/android_hardware_broadcom_libbt"
+pull 9436 "PixelOS-AOSP/android_hardware_google_pixel"
+pull 9440 "PixelOS-AOSP/android_hardware_google_pixel-sepolicy"
+pull 9748 "PixelOS-AOSP/android_hardware_interfaces"
+pull 9601 "PixelOS-AOSP/android_hardware_libhardware"
+pull 9607 "PixelOS-AOSP/android_hardware_nxp_nfc"
+pull 9589 "PixelOS-AOSP/android_hardware_st_nfc"
+pull 9508 "PixelOS-AOSP/android_kernel_configs"
+pull 9045 "PixelOS-AOSP/android_packages_apps_Settings"
+pull 9530 "PixelOS-AOSP/android_packages_modules_Bluetooth"
+pull 9518 "PixelOS-AOSP/android_packages_modules_Connectivity"
+pull 9543 "PixelOS-AOSP/android_packages_modules_Permission"
+pull 9608 "PixelOS-AOSP/android_packages_modules_Wifi"
+pull 9540 "PixelOS-AOSP/android_packages_modules_adb"
+pull 9592 "PixelOS-AOSP/android_packages_services_Telecomm"
+pull 9536 "PixelOS-AOSP/android_packages_services_Telephony"
+pull 9609 "PixelOS-AOSP/android_system_bpf"
+pull 9610 "PixelOS-AOSP/android_system_chre"
+pull 9756 "PixelOS-AOSP/android_system_core"
+pull 9611 "PixelOS-AOSP/android_system_libziparchive"
+pull 9585 "PixelOS-AOSP/android_system_logging"
+pull 9603 "PixelOS-AOSP/android_system_media"
+pull 9523 "PixelOS-AOSP/android_system_netd"
+pull 9830 "PixelOS-AOSP/android_system_security"
+pull 9397 "PixelOS-AOSP/android_system_sepolicy"
+pull 9497 "PixelOS-AOSP/android_system_update_engine"
+pull 9413 "PixelOS-AOSP/android_system_vold"
 
-echo "==> Done: cloned all extra_deps and applied all picks."
+echo "==> Done: cloned all extra_deps and applied all pulls."
 
 # ---------------------------------------------------------------------------
 # Finally, check the whole tree for uncommitted changes
